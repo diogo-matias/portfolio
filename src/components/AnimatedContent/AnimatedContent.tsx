f2f9b060bce1a6a29e83f660191e641753339521
Diogo Matias <diogomaccedo200@gmail.com>
2025-03-20 22:39:25 -0300

    .

--- src/components/AnimatedContent/AnimatedContent.tsx
+++ src/components/AnimatedContent/AnimatedContent.tsx
@@ -1,81 +1,90 @@
 import { useRef, useEffect, useState, ReactNode } from "react";
-import { useSpring, animated, SpringConfig } from "@react-spring/web";
+import {
+    useSpring,
+    animated,
+    SpringConfig,
+    AnimatedProps,
+} from "@react-spring/web";
 
 interface AnimatedContentProps {
-  children: ReactNode;
-  distance?: number;
-  direction?: "vertical" | "horizontal";
-  reverse?: boolean;
-  config?: SpringConfig;
-  initialOpacity?: number;
-  animateOpacity?: boolean;
-  scale?: number;
-  threshold?: number;
-  delay?: number;
+    children: any;
+    distance?: number;
+    direction?: "vertical" | "horizontal";
+    reverse?: boolean;
+    config?: SpringConfig;
+    initialOpacity?: number;
+    animateOpacity?: boolean;
+    scale?: number;
+    threshold?: number;
+    delay?: number;
 }
 
 const AnimatedContent: React.FC<AnimatedContentProps> = ({
-  children,
-  distance = 20,
-  direction = "vertical",
-  reverse = false,
-  config = { tension: 50, friction: 25 },
-  initialOpacity = 0,
-  animateOpacity = true,
-  scale = 1,
-  threshold = 0.1,
-  delay = 0,
+    children,
+    distance = 20,
+    direction = "vertical",
+    reverse = false,
+    config = { tension: 50, friction: 25 },
+    initialOpacity = 0,
+    animateOpacity = true,
+    scale = 1,
+    threshold = 0.1,
+    delay = 0,
 }) => {
-  const [inView, setInView] = useState(false);
-  const ref = useRef<HTMLDivElement | null>(null);
+    const [inView, setInView] = useState(false);
+    const ref = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const element = ref.current;
-    if (!element) return;
+    useEffect(() => {
+        const element = ref.current;
+        if (!element) return;
 
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          observer.unobserve(element);
-          setTimeout(() => {
-            setInView(true);
-          }, delay);
-        }
-      },
-      { threshold }
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    observer.unobserve(element);
+                    setTimeout(() => {
+                        setInView(true);
+                    }, delay);
+                }
+            },
+            { threshold }
+        );
+
+        observer.observe(element);
+
+        return () => observer.disconnect();
+    }, [threshold, delay]);
+
+    const directions: Record<"vertical" | "horizontal", string> = {
+        vertical: "Y",
+        horizontal: "X",
+    };
+
+    const springProps = useSpring({
+        from: {
+            transform: `translate${directions[direction]}(${
+                reverse ? `-${distance}px` : `${distance}px`
+            }) scale(${scale})`,
+            opacity: animateOpacity ? initialOpacity : 1,
+        },
+        to: inView
+            ? {
+                  transform: `translate${directions[direction]}(0px) scale(1)`,
+                  opacity: 1,
+              }
+            : undefined,
+        config,
+    });
+
+    return (
+        //@ts-expect-error
+        <animated.div ref={ref} style={springProps}>
+            {children}
+        </animated.div>
     );
-
-    observer.observe(element);
-
-    return () => observer.disconnect();
-  }, [threshold, delay]);
-
-  const directions: Record<"vertical" | "horizontal", string> = {
-    vertical: "Y",
-    horizontal: "X",
-  };
-
-  const springProps = useSpring({
-    from: {
-      transform: `translate${directions[direction]}(${
-        reverse ? `-${distance}px` : `${distance}px`
-      }) scale(${scale})`,
-      opacity: animateOpacity ? initialOpacity : 1,
-    },
-    to: inView
-      ? {
-          transform: `translate${directions[direction]}(0px) scale(1)`,
-          opacity: 1,
-        }
-      : undefined,
-    config,
-  });
-
-  return (
-    <animated.div ref={ref} style={springProps}>
-      {children}
-    </animated.div>
-  );
 };
+
+type AnimatedDivProps = React.HTMLAttributes<HTMLDivElement> &
+    AnimatedProps<{ style?: React.CSSProperties }>;
 
 export default AnimatedContent;
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,175 +5,187 @@ import { SiLinkedin, SiGithub, SiWhatsapp, SiGmail } from "react-icons/si";
 import AnimatedContent from "@/components/AnimatedContent/AnimatedContent";
 
 export type SendMessageType = {
-  name: string;
-  email: string;
-  subject: string;
-  message: string;
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
 };
 
 export default function Contact() {
-  const [inputsValue, setInputsValue] = useState<SendMessageType>({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const [statusMessage, setStatusMessage] = useState({ message: "", type: "" });
-
-  async function sendMessage(e) {
-    e.preventDefault();
-
-    setStatusMessage({
-      message: "Enviando...",
-      type: "loading",
+    const [inputsValue, setInputsValue] = useState<SendMessageType>({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
     });
 
-    try {
-      const data = await fetch(
-        "https://site-backend-murex.vercel.app/contact",
-        {
-          body: JSON.stringify(inputsValue),
-          headers: {
-            "Content-Type": "application/json",
-            mode: "no-cors",
-          },
-          method: "POST",
+    const [statusMessage, setStatusMessage] = useState({
+        message: "",
+        type: "",
+    });
+
+    async function sendMessage(e: any) {
+        e.preventDefault();
+
+        setStatusMessage({
+            message: "Enviando...",
+            type: "loading",
+        });
+
+        try {
+            const data = await fetch(
+                "https://site-backend-murex.vercel.app/contact",
+                {
+                    body: JSON.stringify(inputsValue),
+                    headers: {
+                        "Content-Type": "application/json",
+                        mode: "no-cors",
+                    },
+                    method: "POST",
+                }
+            );
+
+            setStatusMessage({
+                message: "Enviado com sucesso!",
+                type: "sucess",
+            });
+        } catch (error) {
+            setStatusMessage({
+                message: "Erro ao enviar mensagem, tente novamente!",
+                type: "error",
+            });
         }
-      );
-
-      setStatusMessage({ message: "Enviado com sucesso!", type: "sucess" });
-    } catch (error) {
-      setStatusMessage({
-        message: "Erro ao enviar mensagem, tente novamente!",
-        type: "error",
-      });
-    }
-  }
-
-  function handleChange(key: string, value: string) {
-    setInputsValue((currentValue) => {
-      return {
-        ...currentValue,
-        [key]: value,
-      };
-    });
-  }
-
-  function onChange(e: any) {
-    const value = e.target?.value;
-    const id = e.target?.id;
-
-    handleChange(id, value);
-    console.log(id);
-  }
-
-  useEffect(() => {
-    console.log(inputsValue);
-  }, [inputsValue]);
-
-  function renderInput(value: string, placeholder: string, id: string) {
-    return (
-      <input
-        id={id}
-        placeholder={placeholder}
-        onChange={onChange}
-        className="border-[0.5px] border-gray-500 mt-5 rounded-3xl px-4 py-2 text-sm outline-0 shadow-lg"
-      />
-    );
-  }
-  function renderTextField(value: string, placeholder: string, id: string) {
-    return (
-      <textarea
-        id={id}
-        placeholder={placeholder}
-        onChange={onChange}
-        className="border-[0.5px] mt-5 border-gray-500 rounded-3xl px-4 py-4 text-sm outline-0 h-40 shadow-lg"
-      />
-    );
-  }
-
-  function renderLabel() {
-    let style = "";
-
-    switch (statusMessage.type) {
-      case "sucess":
-        style = "text-green-600";
-        break;
-      case "error":
-        style = "text-red-600";
-        break;
     }
 
+    function handleChange(key: string, value: string) {
+        setInputsValue((currentValue) => {
+            return {
+                ...currentValue,
+                [key]: value,
+            };
+        });
+    }
+
+    function onChange(e: any) {
+        const value = e.target?.value;
+        const id = e.target?.id;
+
+        handleChange(id, value);
+        console.log(id);
+    }
+
+    useEffect(() => {
+        console.log(inputsValue);
+    }, [inputsValue]);
+
+    function renderInput(value: string, placeholder: string, id: string) {
+        return (
+            <input
+                id={id}
+                placeholder={placeholder}
+                onChange={onChange}
+                className="border-[0.5px] border-gray-500 mt-5 rounded-3xl px-4 py-2 text-sm outline-0 shadow-lg"
+            />
+        );
+    }
+    function renderTextField(value: string, placeholder: string, id: string) {
+        return (
+            <textarea
+                id={id}
+                placeholder={placeholder}
+                onChange={onChange}
+                className="border-[0.5px] mt-5 border-gray-500 rounded-3xl px-4 py-4 text-sm outline-0 h-40 shadow-lg"
+            />
+        );
+    }
+
+    function renderLabel() {
+        let style = "";
+
+        switch (statusMessage.type) {
+            case "sucess":
+                style = "text-green-600";
+                break;
+            case "error":
+                style = "text-red-600";
+                break;
+        }
+
+        return (
+            <label className={twMerge(style, "mt-2 text-sm")} htmlFor="form">
+                {statusMessage.message}
+            </label>
+        );
+    }
+
+    function renderContent() {
+        return (
+            <div className="flex flex-col py-5 px-6">
+                <AnimatedContent>
+                    <h1 className="text-4xl tracking-tighter">
+                        Mandar <br />{" "}
+                        <span className="font-semibold text-cyan">
+                            mensagem
+                        </span>
+                    </h1>
+                </AnimatedContent>
+                <AnimatedContent delay={250}>
+                    <form
+                        id="form"
+                        className="flex flex-col"
+                        onSubmit={sendMessage}
+                    >
+                        {renderInput("", "Nome", "name")}
+                        {renderInput("", "Email", "email")}
+                        {renderInput("", "Assunto", "subject")}
+                        {renderTextField("", "Mensagem", "message")}
+                        <button
+                            className="cursor-pointer border-[0.5px] mt-5 border-gray-500 bg-black text-white rounded-3xl px-4 py-2 text-sm shadow-lg"
+                            onClick={sendMessage}
+                            type="submit"
+                        >
+                            Enviar
+                        </button>
+                        {renderLabel()}
+                        {renderContactInfo()}
+                    </form>
+                </AnimatedContent>
+            </div>
+        );
+    }
+
+    function openLink(link: string) {
+        window.open(link, "_blank");
+    }
+
+    function renderContactInfo() {
+        const links = {
+            github: "https://github.com/diogo-matias",
+            linkedIn: "https://www.linkedin.com/in/diogo-matias-profile/",
+            whatsApp: "https://api.whatsapp.com/send?phone=5551994661334",
+            email: "mailto:diogomaccedo200@gmail.com",
+        };
+
+        return (
+            <div className="mt-5">
+                <AnimatedContent delay={500}>
+                    <div className="flex gap-4 text-4xl mb-4 cursor-pointer">
+                        <SiLinkedin onClick={() => openLink(links.linkedIn)} />
+                        <SiGithub onClick={() => openLink(links.github)} />
+                        <SiWhatsapp onClick={() => openLink(links.whatsApp)} />
+                        <SiGmail onClick={() => openLink(links.email)} />
+                    </div>
+                    <p>diogomaccedo200@gmail.com</p>
+                    <p>+55 (51) 99466-1334</p>
+                </AnimatedContent>
+            </div>
+        );
+    }
+
     return (
-      <label className={twMerge(style, "mt-2 text-sm")} htmlFor="form">
-        {statusMessage.message}
-      </label>
+        <div className="from-[var(--background-gradiend)] bg-gradient-to-tl min-h-[100vh] pt-40">
+            <Header />
+            <div className="container mx-auto max-w-sm ">{renderContent()}</div>
+        </div>
     );
-  }
-
-  function renderContent() {
-    return (
-      <div className="flex flex-col py-5 px-6">
-        <AnimatedContent>
-          <h1 className="text-4xl tracking-tighter">
-            Mandar <br />{" "}
-            <span className="font-semibold text-cyan">mensagem</span>
-          </h1>
-        </AnimatedContent>
-        <AnimatedContent delay={250}>
-          <form id="form" className="flex flex-col" onSubmit={sendMessage}>
-            {renderInput("", "Nome", "name")}
-            {renderInput("", "Email", "email")}
-            {renderInput("", "Assunto", "subject")}
-            {renderTextField("", "Mensagem", "message")}
-            <button
-              className="cursor-pointer border-[0.5px] mt-5 border-gray-500 bg-black text-white rounded-3xl px-4 py-2 text-sm shadow-lg"
-              onClick={sendMessage}
-              type="submit"
-            >
-              Enviar
-            </button>
-            {renderLabel()}
-            {renderContactInfo()}
-          </form>
-        </AnimatedContent>
-      </div>
-    );
-  }
-
-  function openLink(link: string) {
-    window.open(link, "_blank");
-  }
-
-  function renderContactInfo() {
-    const links = {
-      github: "https://github.com/diogo-matias",
-      linkedIn: "https://www.linkedin.com/in/diogo-matias-profile/",
-      whatsApp: "https://api.whatsapp.com/send?phone=5551994661334",
-      email: "mailto:diogomaccedo200@gmail.com",
-    };
-
-    return (
-      <div className="mt-5">
-        <AnimatedContent delay={500}>
-          <div className="flex gap-4 text-4xl mb-4 cursor-pointer">
-            <SiLinkedin onClick={() => openLink(links.linkedIn)} />
-            <SiGithub onClick={() => openLink(links.github)} />
-            <SiWhatsapp onClick={() => openLink(links.whatsApp)} />
-            <SiGmail onClick={() => openLink(links.email)} />
-          </div>
-          <p>diogomaccedo200@gmail.com</p>
-          <p>+55 (51) 99466-1334</p>
-        </AnimatedContent>
-      </div>
-    );
-  }
-
-  return (
-    <div className="from-[var(--background-gradiend)] bg-gradient-to-tl min-h-[100vh] pt-40">
-      <Header />
-      <div className="container mx-auto max-w-sm ">{renderContent()}</div>
-    </div>
-  );
 }
